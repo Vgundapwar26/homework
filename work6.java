b135111aa084c8cf574aab0945e0fc7df2fe4be4
root <root@ip-172-31-1-232.ap-south-1.compute.internal>
2020-02-19 10:54:51 +0000

    work6 & work7 added through linux master

---
+++ work6.java
@@ -0,0 +1,5 @@
+class work6{
+	public static void main(string args[]){
+		system.out.println ("work6 added through master linux");
+	}
+}
---
+++ work7.java
@@ -0,0 +1,5 @@
+class work7{
+	public static void main(string args[]){
+		system.out.println ("work7 added through master linux");
+	}
+}
